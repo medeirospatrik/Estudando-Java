8034bef4ae5a5f12d2bd588d42a4aa144277ace4
Patrik Medeiros <medeirospatrik2@gmail.com>
2025-02-08 21:52:10 -0300

    comit inicial, criando primeira controller

---
+++ src/main/java/com/tricodev/tricofood/TricofoodApplication.java
@@ -0,0 +1,13 @@
+package com.tricodev.tricofood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricofoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TricofoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tricodev/tricofood/TricofoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tricodev.tricofood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TricofoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
